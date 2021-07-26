52cc3513e8bebd6f99996beded9b1bd205b58353
rodriguesrenato <renato.rodrigues.rfr@gmail.com>
2021-07-26 00:39:56 -0300

    feat: helper functions + additional print functions

--- src/tools.cpp
+++ src/tools.cpp
@@ -1,24 +1,100 @@
 #include "tools.h"
+#include <iomanip>   // std::setprecision, std::setw
 #include <iostream>
 
-using Eigen::VectorXd;
 using Eigen::MatrixXd;
+using Eigen::VectorXd;
 using std::vector;
 
 Tools::Tools() {}
 
 Tools::~Tools() {}
 
+// Calculate and return the Root Mean Squared Error
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-   * TODO: Calculate the RMSE here.
-   */
+  // Initialize root mean squared error vector
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+
+  // Check if estimation vector is valid
+  if (estimations.size() != ground_truth.size() || estimations.size() == 0) {
+    Print("CalculateRMSE","Invalid estimation or ground_truth data");
+    return rmse;
+  }
+
+  // Accumulate squared residuals
+  for (unsigned int i = 0; i < estimations.size(); ++i) {
+    VectorXd residual = estimations[i] - ground_truth[i];
+
+    // Square residual values and accumulate it in the rmse vector
+    residual = residual.array() * residual.array();
+    rmse += residual;
+  }
+
+  // Calculate the mean
+  rmse = rmse / estimations.size();
+
+  // Calculate the squared root
+  rmse = rmse.array().sqrt();
+
+  return rmse;
 }
 
-MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-   * TODO:
-   * Calculate a Jacobian here.
-   */
+// Calculate and return the Jacobian matrix
+MatrixXd Tools::CalculateJacobian(const VectorXd &x_state) {
+  MatrixXd Hj(3,4);
+
+  // Recover state parameters
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+
+  // Pre-compute a set of terms to avoid repeated calculation
+  float c1 = px*px+py*py;
+  float c2 = std::sqrt(c1);
+  float c3 = (c1*c2);
+
+  // Check division by zero
+  if (std::fabs(c1) < 0.00001) {
+    Print("CalculateJacobian","Error - Division by Zero");
+    return Hj;
+  }
+
+  // Compute the Jacobian matrix
+  Hj << (px/c2), (py/c2), 0, 0,
+      -(py/c1), (px/c1), 0, 0,
+      py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
+
+  return Hj;
+}
+
+// Print a message on cout with the pattern "[origin] message"
+void Tools::Print(std::string o, std::string m){
+  std::cout << "[" << o << "] " << m << std::endl;
+}
+
+// Print state vector and covariance matrix on cout
+void Tools::Print(Eigen::VectorXd rmse, Eigen::VectorXd x, Eigen::MatrixXd P){
+  std::cout << std::fixed << std::setprecision(3);
+  
+  // Print rmse
+  std::cout << "rmse:";
+  for(int i=0;i<rmse.size();i++){
+    std::cout << std::setw(7) << rmse(i) << "" ;
+  }
+
+  // Print state x
+  std::cout << "\tx:";
+  for(int i=0;i<x.size();i++){
+    std::cout << std::setw(7) << x(i) << "" ;
+  }
+
+  // Print covariances P
+  std::cout << "\tP:";
+  for(int i=0;i<P.size();i++){
+    std::cout << std::setw(6) << P(i) << " " ;
+  }
+  std::cout << std::endl;
 }
--- src/tools.h
+++ src/tools.h
@@ -6,27 +6,24 @@
 
 class Tools {
  public:
-  /**
-   * Constructor.
-   */
+  // Constructor.
   Tools();
 
-  /**
-   * Destructor.
-   */
-  virtual ~Tools();
-
-  /**
-   * A helper method to calculate RMSE.
-   */
+  // Destructor
+  ~Tools();
+  
+  // Helper method to calculate RMSE.
   Eigen::VectorXd CalculateRMSE(const std::vector<Eigen::VectorXd> &estimations, 
                                 const std::vector<Eigen::VectorXd> &ground_truth);
 
-  /**
-   * A helper method to calculate Jacobians.
-   */
+  // Helper method to calculate Jacobians.
   Eigen::MatrixXd CalculateJacobian(const Eigen::VectorXd& x_state);
 
+  // Helper method to print a string message on cout
+  void Print(std::string o, std::string m);
+
+  // Helper method to print the given state vector and convariance matrix on cout
+  void Print(Eigen::VectorXd rmse, Eigen::VectorXd x, Eigen::MatrixXd P);
 };
 
 #endif  // TOOLS_H_
